98417902a8b44d9a60dd40a47850903e165b42b3
Praffessar777 <168579750+Praffessar777@users.noreply.github.com>
2025-09-08 18:52:57 +0300

    Remove unused CustomerCabinetSection import
    Removed import for CustomerCabinetSection.

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from "../components/sections/HeroSection";
 import { AdvantagesHeroSection } from "../components/sections/AdvantagesHeroSection";
-import { CustomerCabinetSection } from "../components/sections/CustomerCabinetSection";
 import { ImportantDocumentsSection } from "../components/sections/ImportantDocumentsSection";
 
 export function HomePage() {
